755e141990d119eb33a1675cb58e70e8a0b6c0bf
lisellelogan <liselle.logan@yahoo.com>
2020-12-20 12:55:16 -0500

    created seed file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create(name: "Beemo", email: "beemothecorgi@dog.com", password: "corgibutts")
+
+User.create(name: "Seymour", email: "seymourlogan123@dog.com" password: "beaglesrule!")
+
+User.create(name: "Charlie", email: "thegoldendoodlecharles@dog.com" password: "goldieboy007")
