b69745e0162702bbc8ab25361373443f7a058868
lisellelogan <liselle.logan@yahoo.com>
2020-12-20 16:10:13 -0500

    made edit to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 beemo = User.create(name: "Beemo", email: "beemothecorgi@dog.com", password: "corgibutts")
-
+JournalEntry.create(date: "December 10th 2020", content: "Today was a lazy day.", user: beemo)
+JournalEntry.create(date: "December 11th 2020", content: "Today was a productive day.", user: beemo)
