a7710846b86f5358dbf865fd24383b5b05e89c6e
lisellelogan <liselle.logan@yahoo.com>
2020-12-25 16:01:42 -0500

    created helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,16 @@ class UsersController < ApplicationController
         end
    end
 
+   #helper methods
+    helpers do 
+        def logged_in? 
+            !!session[:user_id]
+        end
+
+        def current_user
+            User.find(session[:user_id])
+        end
+    end
 
     #process form + create user
     ##creating the user AND logging them in
