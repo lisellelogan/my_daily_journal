1c7fc7b6e79f321926a194cb824def3a3abc1f1c
lisellelogan <liselle.logan@yahoo.com>
2020-12-27 17:23:02 -0500

    added to seeds file

--- config.ru
+++ config.ru
@@ -4,6 +4,6 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use Rack::MethodOverride
+use Rack::MethodOverride #in order to send patch and delete requests
 run ApplicationController
 use UsersController
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 beemo = User.create(name: "Beemo", email: "beemothecorgi@dog.com", password: "corgibutts")
-JournalEntry.create(date: "December 10th 2020", content: "Today was a lazy day.", user: beemo)
-JournalEntry.create(date: "December 11th 2020", content: "Today was a productive day.", user: beemo)
+JournalEntry.create(date: "December 10th 2020", content: "Today was a lazy day.", user_id: beemo.id)
+JournalEntry.create(date: "December 11th 2020", content: "Today was a productive day.", user_id: beemo.id)
+
+seymour = User.create(name: "Seymour", email: "seymourthebeagle@dog.com", password: "beaglesrule")
+JournalEntry.create(date: "December 12th 2020", content: "I went for three walks today!", user_id: seymour.id)
+JournalEntry.create(date: "December 13th 2020", content: "It rained all day.", user_id: seymour.id)
