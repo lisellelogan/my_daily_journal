a7b0f4fb49f019a77bfaa38cecbdf5a5f41472ce
lisellelogan <liselle.logan@yahoo.com>
2020-12-28 13:50:30 -0500

    created folder for journal_entries in views and route to create journal entries

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,3 +1,11 @@
 class JournalEntriesController < ApplicationController
-    
+    # get journal_entries/new to render form to create new journal entries
+    get '/jounral_entries/new' do 
+        
+    end
+    # post journal_entries to create new journal entry
+
+    # show route for a journal entry
+
+    # index route for all journal entries
 end
