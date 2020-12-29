96cb4dc13678ee6cb08149c8201bf9f6cbdbbea2
lisellelogan <liselle.logan@yahoo.com>
2020-12-28 21:04:41 -0500

    created and saved new journal entry

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -4,6 +4,19 @@ class JournalEntriesController < ApplicationController
         erb :'journal_entries/new'
     end
     # post journal_entries to create new journal entry
+    post '/journal_entries' do 
+        #create new journal entry and save it to db
+        #only want to save entry if it has content
+        if !logged_in?
+            redirect '/'  
+        end
+        #only want to create entry if user is logged in
+        if params[:content] != ""
+            @journal_entry = JournalEntry.crate(params)
+        else
+            redirect '/journal_entries/new'
+        end
+    end
 
     # show route for a journal entry
 
