b6aa48d765d99d5cb26a9121044951cd7c53a168
lisellelogan <liselle.logan@yahoo.com>
2021-01-01 13:55:21 -0500

    added 'sinatra-flash' gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "therapy_session"
+    register Sinatra::Flash
   end
 
   helpers do 
